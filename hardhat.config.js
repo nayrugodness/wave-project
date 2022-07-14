42a57a9c7b0e348d6408347c8a0d74c63798b634
Nayru <nanualexandraramirez@gmail.com>
2022-07-14 14:32:58 -0500

    adding my alchemy api url

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: "YOUR_ALCHEMY_API_URL",
+      url: "https://eth-rinkeby.alchemyapi.io/v2/A5BzofeCqZ8aIc-oTNWJeenMlH-nZpSS",
       accounts: ["YOUR_PRIVATE_RINKEBY_ACCOUNT_KEY"]
     },
   },
