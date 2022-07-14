c0d85b2895ff0e482a7117bbf3e2e789fb53e410
Nayru <nanualexandraramirez@gmail.com>
2022-07-14 14:35:42 -0500

    making hardhat.config.js secure

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
+// Import and configure dotenv
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/A5BzofeCqZ8aIc-oTNWJeenMlH-nZpSS",
-      accounts: ["c92f7a659c5a6014da5a6f3e88832254bdf3a95672a8bd8825da30678ab32b2f"]
+      // This value will be replaced on runtime
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
