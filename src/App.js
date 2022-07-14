2bb026bb9ed221373dd9e48532b35c13418e1cec
Nayru <nanualexandraramirez@gmail.com>
2022-07-14 14:59:28 -0500

    adding connect wallet button

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  /*
-  * Just a state variable we use to store our user's public wallet.
-  */
   const [currentAccount, setCurrentAccount] = useState("");
 
   const checkIfWalletIsConnected = async () => {
@@ -18,20 +15,38 @@ const App = () => {
         console.log("We have the ethereum object", ethereum);
       }
 
-      /*
-      * Check if we're authorized to access the user's wallet
-      */
       const accounts = await ethereum.request({ method: "eth_accounts" });
 
       if (accounts.length !== 0) {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
-        setCurrentAccount(account)
+        setCurrentAccount(account);
       } else {
         console.log("No authorized account found")
       }
     } catch (error) {
       console.log(error);
+    }
+  }
+
+  /**
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
     }
   }
 
@@ -43,7 +58,7 @@ const App = () => {
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">
-          👋 Hey there!
+        👋 Hey there!
         </div>
 
         <div className="bio">
@@ -53,8 +68,18 @@ const App = () => {
         <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+
+        {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
-    );
-  }
+  );
+}
+
 export default App
